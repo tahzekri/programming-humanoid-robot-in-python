17377af32796dd94d5624333d605ddd3461454d9
tahzekri <zekri@campus.tu-berlin.de>
2023-06-06 21:14:16 +0200

    defs for client edited

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -7,6 +7,10 @@
 '''
 
 import weakref
+from threading import Thread
+from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client
+import numpy as np
 
 class PostHandler(object):
     '''the post hander wraps function to be excuted in paralle
@@ -17,10 +21,16 @@ class PostHandler(object):
     def execute_keyframes(self, keyframes):
         '''non-blocking call of ClientAgent.execute_keyframes'''
         # YOUR CODE HERE
+        main = Thread(target=self.proxy.execute_keyframes,args=[keyframes])	
+        # need to start it then	
+        main.start()
 
     def set_transform(self, effector_name, transform):
         '''non-blocking call of ClientAgent.set_transform'''
         # YOUR CODE HERE
+        main = Thread(target=self.proxy.set_transform,args=[effector_name, transform])
+        # need to start it then
+        main.start()
 
 
 class ClientAgent(object):
@@ -28,36 +38,44 @@ class ClientAgent(object):
     '''
     # YOUR CODE HERE
     def __init__(self):
+        # https://docs.python.org/3/library/xmlrpc.client.html and help of Kommilitonen
         self.post = PostHandler(self)
+        self.s = xmlrpc.client.ServerProxy('http://localhost:9999')
     
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.s.get_angle(joint_name)
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        return self.s.set_angle(joint_name, angle)
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.s.get_posture()
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        return self.s.execute_keyframes(keyframes)
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        return np.asarray(self.s.get_transform(name))
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        return self.s.set_transform(effector_name, transform.tolist())	
 
 if __name__ == '__main__':
     agent = ClientAgent()
